32509867144e36688899933df59f00a29c18dc09
CoolSid <hackersiddhant1@gmail.com>
2026-03-16 14:41:57 +0530

    added docker

--- main.py
+++ main.py
@@ -32,3 +32,7 @@ def recommend_products(request: FilterRequest):
 @app.get("/")
 def root():
     return {"message": "Website is live"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
